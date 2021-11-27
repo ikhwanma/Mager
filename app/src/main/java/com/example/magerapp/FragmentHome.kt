500c5829fc77c7344a50ce101fe445b1ffd52095
auliame <auliaramadhani115@gmail.com>
2021-11-27 19:35:47 +0700

    layout halaman utama blm sempurna

--- app/src/main/java/com/example/magerapp/FragmentHome.kt
+++ app/src/main/java/com/example/magerapp/FragmentHome.kt
@@ -145,6 +145,8 @@ class FragmentHome:Fragment(R.layout.fragment_home) {
                 Toast.makeText(activity, "Gagal", Toast.LENGTH_SHORT).show()
             }
 
+            // apalu 
+
         })
     }
 }
