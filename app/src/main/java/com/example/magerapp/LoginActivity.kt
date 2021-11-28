e9b855a966669eafaf390520c4ca7e674ac82498
USER <ikhwan749@gmail.com>
2021-11-28 22:39:29 +0700

    Login Fix

--- app/src/main/java/com/example/magerapp/LoginActivity.kt
+++ app/src/main/java/com/example/magerapp/LoginActivity.kt
@@ -15,6 +15,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var inputEmail:EditText
     private lateinit var inputPass:EditText
     private lateinit var btnLogin:Button
+    private lateinit var btnBuatAkun: Button
     private lateinit var remember: CheckBox
     private lateinit var auth: FirebaseAuth
     private lateinit var sharedPreferences: SharedPreferences
@@ -27,11 +28,14 @@ class LoginActivity : AppCompatActivity() {
         inputPass = findViewById(R.id.inputPass)
         btnLogin = findViewById(R.id.btnLogin)
         remember = findViewById(R.id.remember)
+        btnBuatAkun = findViewById(R.id.btnBuatAkun)
 
         sharedPreferences = getSharedPreferences("SHARED_PREF",Context.MODE_PRIVATE)
         isRemember = sharedPreferences.getBoolean("CHECKBOX", false)
 
         auth = FirebaseAuth.getInstance()
+
+        openRegister()
 
         if (isRemember){
             intent = Intent(this,HomeActivity::class.java)
@@ -41,6 +45,12 @@ class LoginActivity : AppCompatActivity() {
             login()
         }
 
+    }
+
+    private fun openRegister(){
+        btnBuatAkun.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 
     private fun login() {
@@ -53,20 +63,48 @@ class LoginActivity : AppCompatActivity() {
         val email = inputEmail.text.toString()
         val pass = inputPass.text.toString()
         val checked = remember.isChecked
+        val cek = isValidString(email)
 
         val editor:SharedPreferences.Editor = sharedPreferences.edit()
         editor.putString("EMAIL",email)
         editor.putString("PASSWORD",pass)
         editor.putBoolean("CHECKBOX",checked)
         editor.apply()
-
+        if (email.isEmpty()){
+            inputEmail.setError("Email Tidak Boleh Kosong")
+            inputEmail.requestFocus()
+            return
+        }
+        if (!cek){
+            inputEmail.setError("Email Tidak Sesuai Format")
+            inputEmail.requestFocus()
+            return
+        }
+        if (pass.isEmpty()){
+            inputPass.setError("Password Tidak Boleh Kosong")
+            inputPass.requestFocus()
+            return
+        }
         auth.signInWithEmailAndPassword(email,pass).addOnSuccessListener {
-            Toast.makeText(this,"Berhasil",Toast.LENGTH_SHORT).show()
             startActivity(Intent(this,HomeActivity::class.java))
         }.addOnFailureListener {
-            Toast.makeText(this,"Gagal",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Email Atau Password Yang Anda Masukkan Salah, Silakan Coba Lagi",Toast.LENGTH_SHORT).show()
         }
     }
+
+    fun isValidString(str: String): Boolean{
+        val EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                    "\\@" +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                    "(" +
+                    "\\." +
+                    "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                    ")+"
+        )
+        return EMAIL_ADDRESS_PATTERN.matcher(str).matches()
+    }
+
 
 
 }
